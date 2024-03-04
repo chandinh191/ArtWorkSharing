882e9e33d11af291ca647b40b14d9fe7b5edddb6
chanhday <trieudoublenguyen@gmail.com>
2024-03-04 08:45:20 +0700

    Update ApplicationUser.cs

--- AWS_BusinessObjects/Identity/ApplicationUser.cs
+++ AWS_BusinessObjects/Identity/ApplicationUser.cs
@@ -11,8 +11,6 @@ namespace AWS_BusinessObjects.Identity
     public class ApplicationUser : IdentityUser
     {
         public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
 
         public virtual IList<AdminAccount>? AdminAccounts { get; set; }
