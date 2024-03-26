653659059374e4b88bc980abce613d67625bc39d
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2024-03-26 17:19:53 +0700

    fix wishlist

--- ArtWorkSharingAPI/Controllers/ArtWorkController.cs
+++ ArtWorkSharingAPI/Controllers/ArtWorkController.cs
@@ -26,7 +26,7 @@ namespace ArtWorkSharingAPI.Controllers
             var artWorks = _artWorkService.GetAll();           
             return Ok(artWorks);
         }
-        [Authorize]
+        //[Authorize]
         [HttpGet("GetById")]
         public IActionResult GetById(Guid id)
         {
