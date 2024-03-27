74ce68d65f3d786c247b497adad95b1b350ed50c
zimbear3103 <kiendhduc@gmail.com>
2024-03-27 21:32:25 +0700

    validate Category

--- AWS_BusinessObjects/Entities/Category.cs
+++ AWS_BusinessObjects/Entities/Category.cs
@@ -14,7 +14,7 @@ namespace AWS_BusinessObjects.Entities
     {
         [Required(ErrorMessage = "CategoryName is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters")]
-        [RegularExpression(@"^[A-Za-z\s]{1,50}$", ErrorMessage = "Name can have a maximum of 50 characters and should not contain numbers or special characters.")]
+        [RegularExpression(@"^[A-Za-z0-9\s]{1,50}$", ErrorMessage = "Name can have a maximum of 50 characters and should not contain numbers or special characters.")]
         public string CategoryName { get; set; }
         [Required(AllowEmptyStrings = true, ErrorMessage = "Description is required")]
         [StringLength(500, ErrorMessage = "Description must be at most 500 characters")]
